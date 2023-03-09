97853fbe4b5486c1a1b11b2c1ece1b6e0fdc985d
echoes1205 <111784084+echoes1205@users.noreply.github.com>
2023-03-08 18:41:57 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,4 +3,36 @@ bulmaCarousel.attach("#slider", {
     slidesToShow: 3,
     infinite: true
   });
+
+  var $loginMsg = $('.loginMsg'),
+  $login = $('.login'),
+  $signupMsg = $('.signupMsg'),
+  $signup = $('.signup'),
+  $frontbox = $('.frontbox');
+
+$('#switch1').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.addClass("moving");
+  $signupMsg.toggleClass("visibility");
+
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+$('#switch2').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.removeClass("moving");
+  $signupMsg.toggleClass("visibility");
+
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+setTimeout(function(){
+  $('#switch1').click()
+},1000)
+
+setTimeout(function(){
+  $('#switch2').click()
+},3000)
   
